2e629be2bffe28b1bf032e983dbe8376dea246e8
Animesh-singha <singhaanimesh216@gmail.com>
2026-05-01 16:34:11 +0530

    fix: standard node app for monitoringkshguert

--- pm2-exporter/index.js
+++ pm2-exporter/index.js
@@ -1,2 +1,4 @@
-const exporter = require('pm2-prometheus-exporter');
-exporter();
+// The exporter starts automatically when required.
+// We don't need to call it as a function.
+require('pm2-prometheus-exporter');
+console.log("PM2 Exporter is running...");
